6178e3067c8755fb961947532a8f2214632a24f3
KowsalyaPHP <kowsalya@innovatussytems.com>
2019-10-22 19:06:33 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from "./home/home.component";
+import { HeaderComponent } from './header/header.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+{
+    path: "",
+    component: HomeComponent
+},
+{
+  path: "header",
+  component: HeaderComponent
+},
+{
+  path: "login",
+  component: LoginComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
